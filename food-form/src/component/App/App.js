89c54a3fd41d34d4ba98067b7f4cfe8649209d3f
Jake Johnson <Jakejohnson21@rocketmail.com>
2022-12-10 21:20:00 -0600

    test page switch/reload...?

--- food-form/src/component/App/App.js
+++ food-form/src/component/App/App.js
@@ -3,29 +3,6 @@ import FoodForm from "../FoodForm/FoodForm";
 import PlayForm from "../PlayForm";
 import StayForm from "../StayForm";
 import "./App.css";
-<<<<<<< HEAD
-import { Routes, Route, Link } from "react-router-dom";
-import { useEffect, useState } from "react";
-
-function App() {
-  const baseURL = "https://api.netlify.com/api/v1";
-  const [restaurants, setRestaurants] = useState([]);
-
-  const encode = (data) => {
-    return Object.keys(data)
-      .map(
-        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-      )
-      .join("&");
-  };
-
-  const getSubmissions = (formId) => {
-    fetch(`/${baseURL}/forms/${formId}/submissions`);
-    setRestaurants(getSubmissions("restaurants"));
-  };
-
-  console.log(restaurants);
-=======
 import { useState } from "react";
 
 import { Routes, Route, Link, Navigate } from "react-router-dom";
@@ -33,7 +10,6 @@ import { Routes, Route, Link, Navigate } from "react-router-dom";
 function App() {
   const [dataArray, setDataArray] = useState([]);
 
->>>>>>> forms-working
   return (
     <div className="page">
       <div className="app">
@@ -52,32 +28,18 @@ function App() {
           <Route
             path="/food"
             name="food"
-<<<<<<< HEAD
-            element={<FoodForm encode={encode} />}
-=======
             element={<FoodForm dataArray={dataArray} setData={setDataArray} />}
->>>>>>> forms-working
           />
           <Route
             path="/stay"
             name="stay"
-<<<<<<< HEAD
-            element={<StayForm encode={encode} />}
-=======
             element={<StayForm dataArray={dataArray} setData={setDataArray} />}
->>>>>>> forms-working
           />
           <Route
             path="/play"
             name="play"
-<<<<<<< HEAD
-            element={<PlayForm encode={encode} />}
-          />
-=======
             element={<PlayForm dataArray={dataArray} setData={setDataArray} />}
           />
-          <Route path="*" element={<Navigate to="/" />} />
->>>>>>> forms-working
         </Routes>
 
         <div className="post__content"></div>
