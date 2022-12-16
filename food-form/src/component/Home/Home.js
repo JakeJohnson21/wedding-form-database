8876b13d0908f781632f830c1aa5f6b728147e1a
Jake Johnson <Jakejohnson21@rocketmail.com>
2022-12-16 02:42:58 -0600

    add homepage img

--- food-form/src/component/Home/Home.js
+++ food-form/src/component/Home/Home.js
@@ -3,6 +3,9 @@ function Home() {
     <section className="homepage">
       <span className="heading-title_wrap">
         <h1 className="heading-title">Kyle</h1>
+        <span className="heading-image">
+          {/* <h1 className="heading-date-img">07 14 2023</h1> */}
+        </span>
         <h1 className="heading-title">Farial</h1>
       </span>
 
