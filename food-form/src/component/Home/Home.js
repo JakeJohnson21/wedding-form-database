6ec188fdb6fe3e9bc33ea665decdf3131da31780
Jake Johnson <Jakejohnson21@rocketmail.com>
2022-12-16 16:02:34 -0600

    fix homepage responsiveness

--- food-form/src/component/Home/Home.js
+++ food-form/src/component/Home/Home.js
@@ -1,15 +1,17 @@
+import audubonImage from "../../images/bordered-color-lighterDropShadow-Schlitz.png";
+
 function Home() {
   return (
     <section className="homepage">
-      <span className="heading-title_wrap">
+      <div className="heading-title_wrap">
         <h1 className="heading-title">Kyle</h1>
-        <span className="heading-image">
-          {/* <h1 className="heading-date-img">07 14 2023</h1> */}
-        </span>
-        <h1 className="heading-title">Farial</h1>
-      </span>
+        <div className="heading-sub-wrapper">
+          <img className="heading-image" src={audubonImage} alt="audubon"></img>
+          <h1 className="heading-date">07 14 2023</h1>
+        </div>
 
-      <h1 className="heading-date">07 14 2023</h1>
+        <h1 className="heading-title">Farial</h1>
+      </div>
     </section>
   );
 }
