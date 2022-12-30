848290f743be84952e7ad8fcafd06cd002682af0
Jake Johnson <Jakejohnson21@rocketmail.com>
2022-12-30 02:09:07 -0600

    push for macbook

--- food-form/src/component/Home/Home.js
+++ food-form/src/component/Home/Home.js
@@ -1,9 +1,15 @@
-import audubonImage from "../../images/walkway-b&w-bordered-Schlitz.png";
+import BWImg from "../../images/b-w-bordered-bridge.png";
+import greenGoldImg from "../../images/green-gold-bridge.png";
+import goldenBridge from "../../images/golden-bridge.png";
+import PalerImg from "../../images/paler-bridge-img.png";
+import tableImg from "../../images/table.png";
+import goldbordered from "../../images/gold-bridge-bordered.png";
+import SepiaGreen from "../../images/fadedbrown.png";
 
 function Home() {
   return (
     <section className="page__content">
-      <img className="home__image" src={audubonImage} alt="audubon"></img>
+      <img className="home__image" src={SepiaGreen} alt="audubon"></img>
     </section>
   );
 }
