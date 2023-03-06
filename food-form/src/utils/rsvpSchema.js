73ac5841038df5bf371db14a71a6a5d87e9462b3
Jake Johnson <jake@Jakes-MBP.lan>
2023-03-06 04:05:28 -0600

    removed required from rsvpSchmea for 'picked'

--- food-form/src/utils/rsvpSchema.js
+++ food-form/src/utils/rsvpSchema.js
@@ -3,6 +3,6 @@ import * as yup from "yup";
 export const rsvpSchema = yup.object().shape({
   name: yup.string().required("Your name is required"),
   attending: yup.string().required(),
-  picked: yup.string().required("Please choose your entree"),
+  picked: yup.string(),
   notes: yup.string(),
 });
