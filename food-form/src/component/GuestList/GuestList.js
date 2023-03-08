2ecf6d157c578618f60c3aab5ffa2ce439ab913e
Jake Johnson <Jakejohnson21@rocketmail.com>
2023-03-07 20:33:28 -0600

    add download button

--- food-form/src/component/GuestList/GuestList.js
+++ food-form/src/component/GuestList/GuestList.js
@@ -1,23 +1,23 @@
 import { useEffect, useMemo, useState } from "react";
-// import exportFromJSON from "export-from-json";
+import exportFromJSON from "export-from-json";
 
 import Guest from "./Guest";
 
 function GuestList({ items }) {
-  // const data = items;
-  // const fileName = "download";
-  // const exportType = "xls";
+  const data = items;
+  const fileName = "download";
+  const exportType = "xls";
 
-  // const ExportToExcel = () => {
-  //   exportFromJSON({ data, fileName, exportType });
-  // };
+  const ExportToExcel = () => {
+    exportFromJSON({ data, fileName, exportType });
+  };
 
   return (
     <section className="page guest__page">
       <h2 className="page__title">Guest List</h2>
-      {/* <button className="download__button" onClick={ExportToExcel}>
+      <button className="download__button" onClick={ExportToExcel}>
         <i className="download__icon"></i>
-      </button> */}
+      </button>
       <div className="guest__wrapper">
         {items.map((item) => (
           <Guest
